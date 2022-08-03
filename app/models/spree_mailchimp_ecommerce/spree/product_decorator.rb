c388ac8cbeb6f812a8d2c1a9952aa19c4eebb5fe
Duc T. Lam <ducl_13@yahoo.com>
2022-08-03 11:10:53 -0700

    Get image from CDN

--- app/models/spree_mailchimp_ecommerce/spree/product_decorator.rb
+++ app/models/spree_mailchimp_ecommerce/spree/product_decorator.rb
@@ -24,7 +24,7 @@ module SpreeMailchimpEcommerce
       def active_storage_url
         return "" unless images.any?
 
-        Rails.application.routes.url_helpers.rails_blob_url(images.first&.attachment)
+        images.first&.my_cf_image_url(:large)
       end
 
       def create_mailchimp_product
